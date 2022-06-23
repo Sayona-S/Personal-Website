15647e8ac7c8c2507628aa32db17275d49991575
Sayona-S <108053248+Sayona-S@users.noreply.github.com>
2022-06-23 12:46:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function changeBgColour()
+{
+    document.getElementById("head1").style.backgroundColor="red";
+    
+}
+function chamgeTXTcolour()
+{
+    document.getElementById("head2").style.color="blue";
+}
